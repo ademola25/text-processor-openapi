9239d7c094eb9c99fe047ff31960bdddbaf9b41a
ademola255 <ademola.tijani@boxxtech.io>
2024-06-04 10:36:28 +0100

    adding the project description

--- app.py
+++ app.py
@@ -7,8 +7,15 @@ from project import write_properly, write_the_same_grammar_fixed, summarize
 def main():
     ## adding page title
     st.title("Intelli Text Processing Application")
+    st.caption("A Python-based application leveraging the OpenAI API and Transformers \
+    library to create an agent that assists non-native English speakers in improving their written English. \
+    The application does not involve model training or fine-tuning but focuses on effective prompt engineering using the \
+    Retrieval-Augmented Generation (RAG) approach when needed. The agent will provide three primary functionalities below:")
+    st.write('1. Write Properly: Enhances both the grammar and style of the input message.')
+    st.write('2. Correct Grammar: Correct only the grammatical errors in the input message.')
+    st.write('3. Summarize : Provide a concise summary of the input message"')
     ## creating a side bar to select an option for three services properly from drop down
-    function_choice = st.sidebar.selectbox("Select a function", ("Write Properly", "Fix Grammar", "Summarize"))
+    function_choice = st.sidebar.selectbox("Select a function from this drop down", ("Write Properly", "Fix Grammar", "Summarize"))
     ## text box for user input message 
     input_message = st.text_area("Enter your message")
     ## creating button to initiate the process
